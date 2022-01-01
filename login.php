1fba537d44a2a977fd9702c89b7621c3fb2d363a
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-01 19:24:12 +0100

    invalid post query added

--- login.php
+++ login.php
@@ -7,7 +7,17 @@ $query = $_SERVER["REQUEST_METHOD"];
 if($query == "POST")
 {
   // invalid post query
-  
+  if(empty($_POST["email"]) || empty($_POST["password"]))
+  {
+      exit(json_encode(array(
+          "successfull" => "0"
+      )));
+  }
+   
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+
+
 
 }
 
