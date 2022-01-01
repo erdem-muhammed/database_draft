56b3cf3f895f91e1fc7bc9bb69b682c8d5cc8f3b
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-01 20:15:02 +0100

    cors headers added

--- login.php
+++ login.php
@@ -5,7 +5,8 @@ if (isset($_SERVER['HTTP_ORIGIN']))
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');
 }
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
 include("register.php");
 
