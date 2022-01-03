188ea22add8320f778e68f74cf26f4e0b89e6072
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-03 18:58:58 +0100

    ' added

--- login.php
+++ login.php
@@ -41,7 +41,7 @@ if($query == "POST")
       
       if($resultcontent["password"] == $password) // if the password is correct too
       $token = bin2hex(random_bytes(20));
-      $query = "UPDATE register_info SET token = " . $token . " WHERE id = " . $resultcontent["id"];
+      $query = "UPDATE register_info SET token = '" . $token . "'  WHERE id = " . $resultcontent["id"];
 
       $result2 = $conn->query($query);
       if($result2 === true)
