5c8e690beb43bc05a42f6dc2e9e6986f865c052a
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-06 16:49:55 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,15 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Headers: Content-Type');
 if (isset($_SERVER['HTTP_ORIGIN']))
 {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');
 }
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
+
+
 
 include("register.php");
 
